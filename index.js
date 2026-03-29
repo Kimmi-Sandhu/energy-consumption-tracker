806e9e374cbca9f62545a4a026822e9714d3139b
Ubuntu <ubuntu@ip-172-31-110-139.ap-southeast-2.compute.internal>
2026-03-29 13:27:27 +0000

    final index.js update

--- index.js
+++ index.js
@@ -12,13 +12,16 @@ mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB connected successfully!"))
 .catch(err => console.log("Error:", err));
 
+
 app.get('/', (req, res) => {
   res.send("Energy Consumption Tracker running 🚀");
 });
 
 app.use('/api/tasks', taskRoutes);
 
-app.listen(process.env.PORT, '0.0.0.0', () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 80;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
